3d5c72ca03c0399f1e63f54bd0e97cc36a934c7c
Kiena Dang <ki@Kienas-MacBook-Pro.local>
2022-06-13 23:34:36 -0400

    Update

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -33,7 +33,7 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the airport GeoJSON URL
-let airportData="https://raw.githubusercontent.com/Sukanya807/Mapping_Earthquakes/main/majorAirports.json";
+let airportData="https://raw.githubusercontent.com/Kisdigimonde/Mapping_Earthquakes/Mapping_Single_Points/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data){
